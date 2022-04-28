789cead1aec1f36e0e9200a96dda3034ed22b164
Hiroki Yorimitsu <52403055+hyorimitsu@users.noreply.github.com>
2022-04-28 13:54:19 +0900

    refactor: exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import * as core from "express-serve-static-core";
-import basicAuthMiddleware, {Option as BasicAuthOption, User} from "./basic_auth";
+import basicAuthMiddleware, {Option as BasicAuthOption} from "./basic_auth";
 import ipFilterMiddleware, {Option as IPFilterOption} from "./ip_filter";
 
 type Option = {
@@ -13,10 +13,9 @@ const useAccessControlMiddleware = (app: core.Express, option: Option) => {
         ipFilterOption,
     } = option;
 
-    if (ipFilterOption) app.use('*', ipFilterMiddleware(ipFilterOption));
-    if (basicAuthOption) app.use('*', basicAuthMiddleware(basicAuthOption));
+    if (ipFilterOption) app.use(ipFilterMiddleware(ipFilterOption));
+    if (basicAuthOption) app.use(basicAuthMiddleware(basicAuthOption));
 };
 
 export default useAccessControlMiddleware;
-export {basicAuthMiddleware, ipFilterMiddleware};
-export {Option, BasicAuthOption, User, IPFilterOption};
+export {Option};
